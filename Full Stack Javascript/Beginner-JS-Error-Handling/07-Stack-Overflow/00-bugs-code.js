ec7f9e0516de0cc1b4a86a6ad3a17de8a058afc6
gabaci72 <serianonim@gmail.com>
2025-02-25 14:13:22 +0100

    finding documantation and stack overflow web site

---
+++ Full Stack Javascript/Beginner-JS-Error-Handling/07-Stack-Overflow/00-bugs-code.js
@@ -0,0 +1,18 @@
+// Returns whether or not the provided string contains a substring of "cake" in it.
+function containsCake(string) {
+  
+}
+
+// Should return true
+console.log("containsCake('I think cake is my soul mate.') returns: " + containsCake('I think cake is my soul mate.'));
+
+// Should return false
+console.log("containsCake('Pie is certainly the coolest dessert.') returns: " + containsCake('Pie is certainly the coolest dessert.'));
+
+/* In the code editor, we’ve included an unfinished function, containsCake(). It takes a string and checks if that string contains the substring 'cake' inside of it. If so, it should return true. If not, it should return false.
+
+Using the information in this Stack Overflow post, implement the functionality of this function.
+
+If you’re stuck on how to translate the answer from this forum into working code, check out the hint.
+
+This function is looking to see if a string contains a specific substring. The answers in the Stack Overflow post all use a generic substring. In order to check specifically for "cake" you will need to change any reference to a substring to "cake" in your code. Other than that, any of the answers in the Stack Overflow post should work.*/
